9f88fd02c2f94004cda2c482fa99b4f498ab2d54
XeniaSZ <xenia.lomowa@yandex.ru>
2022-07-01 20:34:01 +0500

    task 2.1 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -1,24 +1,18 @@
 function calcShipping(sum, min, shipping) {
-    let productsSum = sum; // сумма в корзине
-    let freeShippingMinSum = min; // минимальная цена для бесплатной доставки
-    let shippingPrice = shipping; // стоимость доставки
+    let productsSum = sum; 
+    let freeShippingMinSum = min; 
+    let shippingPrice = shipping; 
 
-    // Задание №2.1. Рассчитать доставку
+    let shippingSum;
 
-    // создайте переменную shippingSum
-
-    // если productsSum равно 0,
-    // то shippingSum присвоить значение 0
-
-    // если productsSum Больше или равна freeShippingMinSum,
-    // то shippingSum присвоить значение 0
-
-    // если productsSum больше 0 и меньше freeShippingMinSum,
-    // то shippingSum присвоить значение shippingPrice
-
-    // Конец решения задания №2.1.
+    if (productsSum == 0 || productsSum >= freeShippingMinSum) {
+    shippingSum = 0;
+    } else { (productsSum > 0) && (productsSum < freeShippingMinSum) 
+    shippingSum = shippingPrice;
+    }
 
     return shippingSum;
+
 }
 
 function calcDiscount(sum, min, discount) {
